7a8cf8d0cb57d273f16cd1b1a79ecfd4f2200a90
LooninS <hushed55@proton.me>
2025-12-27 14:27:18 +0530

    feat: Added the client-side logic

---
+++ client/chat.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/gorilla/websocket"
+	"log"
+	"net/url"
+	"os"
+	"time"
+)
+
+func connectToEchoServer(serverURL string, username string) error {
+	u := url.URL{Scheme: "ws", Host: serverURL, Path: "/"}
+	fmt.Printf("Connecting to %s\n", u.String())
+	c, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+	if err != nil {
+		return fmt.Errorf("[%s] ✗ Failed to connect to server: %v", time.Now().Format("15:04:05"), err)
+
+	}
+
+	defer c.Close()
+
+	fmt.Printf("[%s] ✓ Connected to server\n", time.Now().Format("15:04:05"))
+
+	err = c.WriteMessage(websocket.TextMessage, []byte(username))
+	if err != nil {
+		return fmt.Errorf("Failed to write to server: %v", err)
+
+	}
+	fmt.Printf("[%s] ✓ Username sent: %s\n", time.Now().Format("15:04:05"), username)
+	fmt.Println("-------------------------------------------")
+	fmt.Println("Listening for messages from server...")
+	fmt.Println("-------------------------------------------\n")
+
+	for {
+		messageType, message, err := c.ReadMessage()
+		if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+			log.Printf("error: %v", err)
+			return err
+		}
+		if err != nil {
+			fmt.Printf("error: %v\n", err)
+			break
+		}
+		if messageType == websocket.TextMessage {
+			fmt.Printf("[%s] ✓ Message received: %s\n", time.Now().Format("15:04:05"), message)
+			fmt.Println("-------------------------------------------")
+
+		}
+
+	}
+	return nil
+
+}
+
+func getUsername() string {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter your username: ")
+	username, _ := reader.ReadString('\n')
+	if len(username) > 0 && username[len(username)-1] == '\n' {
+		username = username[:len(username)-1]
+	}
+	return username
+}
